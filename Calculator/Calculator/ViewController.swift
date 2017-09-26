1e11b1dab4d85cefb4cf9ff7eebbf3b6fb993deb
SHIVAM SHAH <shivam@shivams-imac.local>
2017-09-27 04:40:09 +0530

    Changed UI and added constraints

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func performOperations(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
