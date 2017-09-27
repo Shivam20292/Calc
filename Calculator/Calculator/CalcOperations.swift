7d7d8b03f463b196989faf8f37633d75f0a9e279
SHIVAM SHAH <shivam@shivams-imac.local>
2017-09-27 11:57:14 +0530

    All binary operations added

---
+++ Calculator/Calculator/CalcOperations.swift
@@ -0,0 +1,85 @@
+//
+//  CalcOperations.swift
+//  Calculator
+//
+//  Created by SHIVAM SHAH on 27/09/17.
+//  Copyright © 2017 Centennial College. All rights reserved.
+//
+
+/*
+import Foundation
+
+    class CalcOperations
+        {
+        
+        private var accumulator: Double = 0.0
+        
+            func setOperand (operand: Double) {
+                accumulator = operand
+        }
+        
+        var operations: Dictionary<String,Operation> = [
+    
+        //    "+" : Operation.BinaryOperation({ $0 + $1 }),
+            
+            "+" : Operation.BinaryOperation({ $0 + $1}),
+            
+            "−" : Operation.BinaryOperation({ $0 - $1 }),
+            
+            "×" : Operation.BinaryOperation({ $0 * $1 }),
+            
+            "÷" : Operation.BinaryOperation({ $0 / $1 }),
+            
+            "=": Operation.Equals
+            ]
+        
+        enum Operation
+        {
+            //case Constant
+            case BinaryOperation
+            case Equals
+        }
+        
+            func performOperation (symbol: String) {
+                if let operation = operations[symbol]{
+                
+                    switch operation
+                    {
+                    case .BinaryOperation(let function):
+                        executePendingBinaryOperation()
+                        pending = pendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+                    
+                    case .Equals: break
+                    executePendingBinaryOperation()
+                    
+                    }
+                }
+        }
+        
+        private func executePendingBinaryOperation()
+        {
+            if pending != nil {
+                accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+                pending = nil
+                
+            }
+        }
+        
+        private var pending: pendingBinaryOperationInfo?
+        
+        struct pendingBinaryOperationInfo
+        {
+                      var binaryFunction: (Double, Double) -> Double
+                      var firstOperand: Double
+        }
+        
+        var result: Double {
+            get
+                {
+                    return accumulator
+            }
+        }
+
+}
+
+*/
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,19 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     var numberOnLabel:Double =  0;
     var previousNumber:Double = 0;
     var performOperations = false
     var operation = 0;
     
-    
     @IBOutlet weak var displayLabel: UILabel!
     
     
+   
+ 
     @IBAction func digits(_ sender: UIButton)
-       {
-        
+       
+       
+    {
         if performOperations == true
         {
             displayLabel.text = String(sender.tag-1)
@@ -29,88 +31,91 @@ class ViewController: UIViewController {
             performOperations = false
             
         }
-        
+            
         else
         {
-        displayLabel.text = displayLabel.text! + String (sender.tag-1)
-        numberOnLabel = Double(displayLabel.text!)!
+            displayLabel.text = displayLabel.text! + String (sender.tag-1)
+            numberOnLabel = Double(displayLabel.text!)!
+            
+        }
     }
-    }
-   
-    
-    @IBAction func Operations(_ sender: UIButton)
-    
-    {
-        previousNumber = Double (displayLabel.text!)!
-    
         
-        if displayLabel.text != "" && sender.tag != 11 && sender.tag != 16
+        
+     
+    
+    @IBAction func operate(_ sender: UIButton)
+    {
+       if displayLabel.text != "" && sender.tag != 11 && sender.tag != 16
         {
             
+             previousNumber = Double (displayLabel.text!)!
             
-            if sender.tag == 12             //Division Button with Tag # 12
+            /*
+             
+             
+             if sender.tag == 12             //Division Button with Tag # 12
             {
                 
             }
-                    
+                
             else if sender.tag == 13            // Multiplication Button with Tag # 13
             {
                 
             }
-            
+                
             else if sender.tag == 14           // Substraction Button with Tag # 14
-                {
-                    
+            {
+                
             }
-            
+                
             else if sender.tag == 15            //Addition Button with Tag # 15
-                {
-                    
+            {
+                
             }
-            
+             */
+ 
             operation = sender.tag
             performOperations = true;
             
         }
-            else if sender.tag == 16            //Equals Button with Tag # 16
-            {
-             
-                if operation == 12              //Division
-                {
-                    
-                }
-                
-                else if operation == 13         //Multiplication
-                {
-                    
-                    
-                }
-                else if operation == 14          //Substration
-                {
-                    
-                    
-                }
-                
-                else if operation == 15         //Addition
-                {
-                    displayLabel.text = String (previousNumber + numberOnLabel)
-                }
-        }
         
+        else  if sender.tag == 16            //Equals Button with Tag # 16
+        {
+            
+            if operation == 12              //Division
+            {
+                displayLabel.text = String (previousNumber / numberOnLabel)                }
+                
+           
+            else if operation == 13         //Multiplication
+            {
+                
+                displayLabel.text = String (previousNumber * numberOnLabel)
+            }
+                
+            
+            else if operation == 14          //Substration
+            {
+                displayLabel.text = String (previousNumber - numberOnLabel)
+            }
+                
+            
+            else if operation == 15         //Addition
+            {
+                displayLabel.text = String (previousNumber + numberOnLabel)
+            }
+        }
+            
         else if sender.tag == 11              // Reset everything on click of "C" button
         {
-            displayLabel.text =  "0"
+            displayLabel.text = ""
             previousNumber = 0;
             operation = 0;
             numberOnLabel = 0;
-                
-            }
+            
+        }
         
-    }
-    }
-    
-    
-    
-
+}
+}
 
 
